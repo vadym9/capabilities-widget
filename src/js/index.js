aa1650b76db4168133a6ce7bd8d3031b737bdac9
Vadym <vadym.putrov@chisw.com>
2020-02-05 10:58:54 +0200

    clean

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,38 @@
-// console.log($('div'));
-
-
 $(document).ready(
-  function(){
-    console.log("Document ready");
-      $('.links li').each(function (index){
-        console.log("index = ",index);
-        $(this).attr("id", "link" + index)
-        // console.log($(this));
-      })
-
-      $('.data li').each(function(index){
-        $(this).attr("id","link" + index + "-content")
-      })
-
-      $('.links li').click(function(){
-        console.log();
-        
-        var linkId = $(this).prop('id');
-        console.log(linkId);
-        
-        $('.selected-link').removeAttr('class');
-        // console.log($(this).children());
-        
-        $(this).children().addClass('selected-link')
-        // $(this).addClass('selected-link')
-        $(".data li").slideUp()
-        $("#" + linkId + "-content").slideDown(1000);
-        
-        // $(".links li").not().css("display","none")
-      
-        
-        console.log();
-         
-      })
-      $('.data li').hide();
-      // $('p').append('this is paragraph');
-      $('.links li h2').first().addClass('selected-link')
-      $('.data li').first().slideToggle(1000)
-      // $('[class="more"]').append('all right this is a class')
+  function () {
+    init();
+    $('.links li').click(onClick);
   }
 )
 
+function init() {
+  setIdAllLinks();
+  setIdAllDataBlocks();
+  hideAllAndShowFirstSlide();
+}
 
-// console.log(document);
+function setIdAllLinks() {
+  $('.links li').each(function (index) {
+    $(this).attr("id", "link" + index)
+  })
+}
+
+function setIdAllDataBlocks() {
+  $('.slides li').each(function (index) {
+    $(this).attr("id", "link" + index + "-slide")
+  })
+}
+
+function onClick() {
+  var linkId = $(this).prop('id');
+  $('.selected-link').removeAttr('class');
+  $(this).children().addClass('selected-link')
+  $(".slides li").slideUp()
+  $("#" + linkId + "-slide").slideDown(1000);
+}
+
+function hideAllAndShowFirstSlide() {
+  $('.slides li').hide();
+  $('.links li h2').first().addClass('selected-link');
+  $('.slides li').first().slideToggle(1000);
+}
