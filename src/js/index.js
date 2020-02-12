e1be57627fe3be27c955ce87d8f79829d2a4948d
Vadym <vadym.putrov@chisw.com>
2020-02-12 15:19:29 +0200

    fix pixel perfect

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,38 @@
 $(document).ready(
   function () {
     init();
-    $('.links li').click(onClick);
+    $('.links li').click(onTab);
   }
 )
 
-function init() {
+const init = () => {
   setIdAllLinks();
   setIdAllDataBlocks();
   hideAllAndShowFirstSlide();
 }
 
-function setIdAllLinks() {
+const setIdAllLinks = () => {
   $('.links li').each(function (index) {
     $(this).attr("id", "link" + index)
   })
 }
 
-function setIdAllDataBlocks() {
+const setIdAllDataBlocks = () => {
   $('.slides li').each(function (index) {
     $(this).attr("id", "link" + index + "-slide")
   })
 }
 
-function onClick() {
+function onTab() {
   var linkId = $(this).prop('id');
   $('.selected-link').removeAttr('class');
   $(this).children().addClass('selected-link')
-  $(".slides li").slideUp()
-  $("#" + linkId + "-slide").slideDown(1000);
+  $(".slides li").hide()
+  $("#" + linkId + "-slide").slideToggle();
 }
 
 function hideAllAndShowFirstSlide() {
   $('.slides li').hide();
   $('.links li h2').first().addClass('selected-link');
-  $('.slides li').first().slideToggle(1000);
+  $('.slides li').first().slideToggle();
 }
